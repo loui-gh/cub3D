22ddb464c16af9309047514d4958d306331b7176
loflavel <loflavel@student.42wolfsburg.de>
2022-07-22 23:29:04 +1000

    check plz

--- srcs/raycast_tex.c
+++ srcs/raycast_tex.c
@@ -6,7 +6,7 @@
 /*   By: Loui :) <loflavel@students.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 15:12:06 by jpfannku          #+#    #+#             */
-/*   Updated: 2022/07/22 22:45:06 by Loui :)          ###   ########.fr       */
+/*   Updated: 2022/07/22 23:28:42 by Loui :)          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,6 @@ int	raycast_tex(t_vars *vars)
 	float	ray_dir_y;
 	float 	sideDistX;
 	float 	sideDistY;
-	float 	deltaDistX;
-	float 	deltaDistY;
 	float 	perpWallDist;
 	int 	texY;
 	t_data *fl_c;
@@ -39,46 +37,45 @@ int	raycast_tex(t_vars *vars)
 		{
 		 	ray_dir_x = vars->player->dir_x + 0.9 * vars->player->dir_y * (2 * x / (float)WIDTH - 1);
 			ray_dir_y = vars->player->dir_y + 0.9 * vars->player->dir_x * (2 * x / (float)WIDTH - 1);
-			int mapX = vars->player->pos_x;
-			int mapY = vars->player->pos_y;
-			deltaDistX = sqrt(1 + (ray_dir_y * ray_dir_y) / (ray_dir_x * ray_dir_x));
-			deltaDistY = sqrt(1 + (ray_dir_x * ray_dir_x) / (ray_dir_y * ray_dir_y));
 			int stepX;
 			int stepY;
 
-			int hit = 0;
+			
 			int side;
 			if(ray_dir_x < 0)
 			{
 				stepX = -1;
-				sideDistX = (vars->player->pos_x - mapX) * deltaDistX;
+				sideDistX = 0;
 			}
 			else
 			{
 				stepX = 1;
-				sideDistX = (mapX + 1.0 - vars->player->pos_x) * deltaDistX;
+				sideDistX = (float)(sqrt(1 + (ray_dir_y * ray_dir_y) / (ray_dir_x * ray_dir_x)));
 			}
 			if(ray_dir_y < 0)
 			{
 				stepY = -1;
-				sideDistY = (vars->player->pos_y - mapY) * deltaDistY;
+				sideDistY = 0;
 			}
 			else
 			{
 				stepY = 1;
-				sideDistY = (mapY + 1.0 - vars->player->pos_y) * deltaDistY;
+				sideDistY = (float)(sqrt(1 + (ray_dir_x * ray_dir_x) / (ray_dir_y * ray_dir_y)));
 			}
+			int mapX = vars->player->pos_x;
+			int mapY = vars->player->pos_y;
+			int hit = 0;
 			while(hit == 0)
 			{
 				if(sideDistX < sideDistY)
 				{
-					sideDistX += deltaDistX;
+					sideDistX += sqrt(1 + (ray_dir_y * ray_dir_y) / (ray_dir_x * ray_dir_x));
 					mapX += stepX;
 					side = 0;
 				}
 				else
 				{
-					sideDistY += deltaDistY;
+					sideDistY += sqrt(1 + (ray_dir_x * ray_dir_x) / (ray_dir_y * ray_dir_y));
 					mapY += stepY;
 					side = 1;
 				}
@@ -89,26 +86,33 @@ int	raycast_tex(t_vars *vars)
 			else
 				perpWallDist = (mapY - vars->player->pos_y + (1 - stepY) / 2) / ray_dir_y;
 	
-			int lineHeight = (HEIGHT/ perpWallDist);
-			int drawStart = -lineHeight / 2 + HEIGHT/ 2;
-			if(drawStart < 0) drawStart = 0;
-			int drawEnd = lineHeight / 2 + HEIGHT/ 2;
-			if(drawEnd >= HEIGHT) drawEnd = HEIGHT- 1;
+			int drawStart = -(HEIGHT/ perpWallDist) / 2 + HEIGHT/ 2;
+			if(drawStart < 0) 
+				drawStart = 0;
+			int drawEnd = (HEIGHT/ perpWallDist) / 2 + HEIGHT/ 2;
+			if(drawEnd >= HEIGHT)
+				drawEnd = HEIGHT- 1;
 
 			double wallX;
-			if(side == 0) wallX = vars->player->pos_x + perpWallDist * ray_dir_y;
-			else          wallX = vars->player->pos_y + perpWallDist * ray_dir_x;
+			if(side == 0)
+				wallX = vars->player->pos_x + perpWallDist * ray_dir_y;
+			else
+				wallX = vars->player->pos_y + perpWallDist * ray_dir_x;
 			wallX -= floor(wallX);
 			int texX = wallX * vars->tex->north->height;
-			if(side == 0 && ray_dir_x < 0) texX = vars->tex->north->height - texX - 1;
-			if(side == 1 && ray_dir_y > 0) texX = vars->tex->north->height - texX - 1;
-			double step = 1.0 * vars->tex->north->height / lineHeight;
-			double texPos = (drawStart - HEIGHT/ 2 + lineHeight / 2) * step; //
-			for(int y = drawStart; y < drawEnd; y++)
+			if(side == 0 && ray_dir_x < 0)
+				texX = vars->tex->north->height - texX - 1;
+			if(side == 1 && ray_dir_y > 0)
+				texX = vars->tex->north->height - texX - 1;
+			double step = 1.0 * vars->tex->north->height / (HEIGHT/ perpWallDist);
+			double texPos = (drawStart - HEIGHT/ 2 + (HEIGHT/ perpWallDist) / 2) * step; //
+			int y = drawStart;
+			while( y < drawEnd)
 			{
 				texY = (int)texPos;
 				texPos += step;
 				buffer[y][x] = get_pix_colour(vars->tex->north, texX, texY);
+				y++;
 			}
 			x++;
 			if (x == 31)
